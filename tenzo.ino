8f2331abe185238a58e1d7589c5866870451d94d
Dmitry-Fedotov-Dev <cosmonazic@gmail.com>
2021-03-07 21:19:25 +0300

    create new branch "two gages"

--- tenzo.ino
+++ tenzo.ino
@@ -1,65 +1,38 @@
 #include "HX711.h"
 
+#define DT1  A0
+#define SCK1 A1
+#define DT2  A2
+#define SCK2 A2
 
-
-#define DT  A0
-#define SCK A1
-HX711 scale;
+HX711 scale1;
+HX711 scale2;
 
 bool flag;
-int i;
-float unit;
+float unit1;
+float unit2;
 float following_unit;
-float last_unit;
-float delta;
-
-
 float konvert = 0.035274 * 0.3404255319;
 float calibration_factor = -14.15;
 
-char print_value;
-
 void setup() {
   Serial.begin(9600);
-  scale.begin(DT, SCK);
-  scale.set_scale();
-  scale.tare();
-  scale.set_scale(calibration_factor);
-  Serial.println("Gage is ready to work.");
+  scale1.begin(DT1, SCK1);
+  scale1.set_scale();
+  scale1.tare();
+  scale1.set_scale(calibration_factor);
+  scale2.begin(DT2, SCK2);
+  scale2.set_scale();
+  scale2.tare();
+  scale2.set_scale(calibration_factor);
+  Serial.println("Gages is ready to work.");
 }
 
 void loop() {
-  unit = scale.get_units() * konvert;
-  Serial.print(unit);
-  Serial.println(" x 10^(-6)");
-  following_unit = scale.get_units() * konvert;
-  delta = abs((unit - following_unit) * 100 / unit);
-  delay(1000);
-  if (delta > 100) {
-    last_unit = unit;
-    i++;
-    get_delta(i, last_unit);
-  }
-  else {
-    return 0;
-  }
-}
-
-void get_delta(int i, float last_unit) {
-  flag = true;
-  while (flag == true) {
-    delta = abs((scale.get_units() * konvert - last_unit) * 100 / unit);
-    if (delta > 100) {
-      Serial.print("Expirement № ");
-      Serial.print(i);
-      Serial.print(" ");
-      Serial.print(abs(scale.get_units() * konvert - last_unit));
-      Serial.println(" x 10^(-6)");
-    }
-    else {
-      flag = !flag;
-    }
-  }
-
-  return 0;
+  unit1 = scale1.get_units() * konvert;
+  unit2 = scale2.get_units() * konvert;
+  Serial.print("Gage #1: ")
+  Serial.print(unit1);
+  Serial.print("        Gage #2:");
+  Serial.println(unit2);
 }
